cf985096075dd3708ebc78cd4b021abd51095330
Dave S. Rai <dave.s.rai@gmail.com>
2016-08-25 18:13:14 -0400

    Example of importing modules Example 2-6

--- Learn/Elixir/IntroElixir/combined.ex
+++ Learn/Elixir/IntroElixir/combined.ex
@@ -1,5 +1,9 @@
 defmodule Combined do
+
+	import Convert
+
+
 	def height_to_mph(meters) do
-		Drop.fall_velocity(meters) |> Convert.mps_to_mph
+		Drop.fall_velocity(meters) |> mps_to_mph
 	end
 end
